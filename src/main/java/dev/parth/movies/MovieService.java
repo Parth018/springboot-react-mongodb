6a61b7c03f16a53afdced80a64bedf3dbca8a237
parth <parthprajapati467@gmail.com>
2024-03-31 13:34:45 -0400

    get & post api Commit

---
+++ src/main/java/dev/parth/movies/MovieService.java
@@ -0,0 +1,26 @@
+package dev.parth.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private  MovieRespository movieRespository;
+
+    public List<Movie> allMovies(){
+        return movieRespository.findAll();
+    }
+
+//    public Optional<Movie> singleMovie(ObjectId id){
+//        return movieRespository.findById(id);
+//    }
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRespository.findMovieByImdbId(imdbId);
+    }
+}
