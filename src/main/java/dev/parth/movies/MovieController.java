99f4c2f7e1169c37685dc5975f37308ded1977ae
Parth Prajapati <parthprajapati467@gmail.com>
2024-04-02 16:59:01 -0400

    Update MovieController.java

--- src/main/java/dev/parth/movies/MovieController.java
+++ src/main/java/dev/parth/movies/MovieController.java
@@ -24,6 +24,11 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    // get method for testing only
+    // public ResponseEntity<List<Movie>> getAllMovies(){
+    //     return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    // }
+
 //    @GetMapping("/{id}")
 //    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
 //        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
